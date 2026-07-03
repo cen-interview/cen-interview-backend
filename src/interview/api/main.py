9a02bb46bbb75db2a9d5addc2e898d2805186fe5
ganchisun <minsur0919@gmail.com>
2026-07-03 17:01:30 +0900

    fix: cors 설정 추가

--- src/interview/api/main.py
+++ src/interview/api/main.py
@@ -40,7 +40,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://cen-interview-front.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
